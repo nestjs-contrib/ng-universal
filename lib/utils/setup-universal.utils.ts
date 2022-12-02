fca3daeb5d9ad3d866d10bc67c3e10a97df93306
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2022-12-02 12:22:44 -0500

    chore(): fixing problem with previous merge of try-catch block

--- lib/utils/setup-universal.utils.ts
+++ lib/utils/setup-universal.utils.ts
@@ -1,4 +1,5 @@
 import { Logger } from '@nestjs/common';
+import { StaticProvider } from '@angular/core';
 import { ngExpressEngine } from '@nguniversal/express-engine';
 import {
   default as express,
@@ -17,77 +18,59 @@ export function setupUniversal(app: any, ngOptions: AngularUniversalOptions) {
   try {
     const cacheOptions = getCacheOptions(ngOptions);
 
-<<<<<<< HEAD
-    app.engine('html', (_, options, callback) => {
-      let cacheKey;
-=======
-  app.engine(
-    'html',
-    (
-      _: string,
-      options: { req: Request },
-      callback: (err: Error, html?: string) => void
-    ) => {
-      let cacheKey: string;
->>>>>>> 66dc7bce7457bb3b2544afc51d4c04a7ae1b9b39
-      if (cacheOptions.isEnabled) {
-        const cacheKeyGenerator = cacheOptions.keyGenerator;
-        cacheKey = cacheKeyGenerator.generateCacheKey(options.req);
-        const cacheHtml = cacheOptions.storage.get(cacheKey);
-        if (cacheHtml) {
-          return callback(null, cacheHtml);
+    app.engine(
+      'html',
+      (
+        _: string,
+        options: { req: Request },
+        callback: (err: Error, html?: string) => void
+      ) => {
+        let cacheKey: string;
+        if (cacheOptions.isEnabled) {
+          const cacheKeyGenerator = cacheOptions.keyGenerator;
+          cacheKey = cacheKeyGenerator.generateCacheKey(options.req);
+          const cacheHtml = cacheOptions.storage.get(cacheKey);
+          if (cacheHtml) {
+            return callback(null, cacheHtml);
+          }
         }
+
+        ngExpressEngine({
+          bootstrap: ngOptions.bootstrap,
+          inlineCriticalCss: ngOptions.inlineCriticalCss,
+          providers: [
+            {
+              provide: 'serverUrl',
+              useValue: `${options.req.protocol}://${options.req.get('host')}`
+            },
+            /* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */
+            ...(ngOptions.extraProviders || [])
+          ] as StaticProvider[]
+        })(_, options, (err, html) => {
+          if (err && ngOptions.errorHandler) {
+            return ngOptions.errorHandler({
+              err,
+              html,
+              renderCallback: callback
+            });
+          }
+
+          if (err) {
+            logger.error(err);
+            return callback(err);
+          }
+
+          if (cacheOptions.isEnabled && cacheKey) {
+            cacheOptions.storage.set(cacheKey, html, cacheOptions.expiresIn);
+          }
+          callback(null, html);
+        });
       }
-
-      ngExpressEngine({
-        bootstrap: ngOptions.bootstrap,
-        inlineCriticalCss: ngOptions.inlineCriticalCss,
-        providers: [
-          {
-            provide: 'serverUrl',
-            useValue: `${options.req.protocol}://${options.req.get('host')}`
-          },
-<<<<<<< HEAD
-          ...(ngOptions.extraProviders || [])
-        ]
-      })(_, options, (err, html) => {
-        if (err && ngOptions.errorHandler) {
-          return ngOptions.errorHandler({ err, html, renderCallback: callback });
-=======
-          /* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */
-          ...(ngOptions.extraProviders || [])
-        ] as StaticProvider[]
-      })(_, options, (err, html) => {
-        if (err && ngOptions.errorHandler) {
-          return ngOptions.errorHandler({
-            err,
-            html,
-            renderCallback: callback
-          });
->>>>>>> 66dc7bce7457bb3b2544afc51d4c04a7ae1b9b39
-        }
-
-      if (err) {
-        logger.error(err);
-        return callback(err);
-      }
-
-        if (cacheOptions.isEnabled && cacheKey) {
-          cacheOptions.storage.set(cacheKey, html, cacheOptions.expiresIn);
-        }
-        callback(null, html);
-      });
-<<<<<<< HEAD
-    });
-=======
-    }
-  );
->>>>>>> 66dc7bce7457bb3b2544afc51d4c04a7ae1b9b39
+    );
 
     app.set('view engine', 'html');
     app.set('views', ngOptions.viewsPath);
 
-<<<<<<< HEAD
     // Serve static files
     app.get(
       ngOptions.rootStaticPath,
@@ -95,20 +78,11 @@ export function setupUniversal(app: any, ngOptions: AngularUniversalOptions) {
         maxAge: 600
       })
     );
-  } catch (error) {
-    throw error;
+  } catch(e) {
+    console.error(e);
+    process.exit(1);
   }
-}
-=======
-  // Serve static files
-  app.get(
-    ngOptions.rootStaticPath,
-    express.static(ngOptions.viewsPath, {
-      maxAge: 600
-    })
-  );
 };
->>>>>>> 66dc7bce7457bb3b2544afc51d4c04a7ae1b9b39
 
 export const getCacheOptions = (ngOptions: AngularUniversalOptions) => {
   if (!ngOptions.cache) {
